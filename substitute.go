e181ed4c55da321a4e735a9313fe900d33aed6b3
tetor <tetor.takashi.etou@gmail.com>
2017-10-13 18:53:57 +0900

    change include path

--- substitute.go
+++ substitute.go
@@ -10,7 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 
-	"github.com/guregu/dynamo/internal/exprs"
+	"github.com/tetor/dynamo/internal/exprs"
 )
 
 // subber is a "mixin" for operators for keep track of subtituted keys and values
